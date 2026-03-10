7c071b030da64da3b814cf461ff1ecca2c501a14
zoneherenow <zoneherenow@gmail.com>
2026-03-11 00:58:03 +0800

    Add files via upload

--- zone.js
+++ zone.js
@@ -1,8 +1,15 @@
-function myFunction() {
-    var x = document.getElementById("mySidebar");
-    if (x.className === "sidebar") {
-      x.className += " responsive";
-    } else {
-      x.className = "sidebar";
-    }
+const textElement = document.getElementById('typing-text');
+const textToType = "Oh, hello. You've ventured a little too far. Come back another time.";
+let index = 0;
+
+function typeWriter() {
+  if (index < textToType.length) {
+    textElement.textContent += textToType.charAt(index);
+    index++;
+    // Adjust the delay (e.g., 50ms) as needed
+    setTimeout(typeWriter, 50); 
   }
+}
+
+// Start the animation
+typeWriter();
